00e74efec8b1c7ed973e1d8432aa3132790e2f59
cardosofede <federico.cardoso.e@gmail.com>
2023-07-13 22:52:39 +0200

    (feat) refactor pool names

--- geckoterminal_py/constants.py
+++ geckoterminal_py/constants.py
@@ -8,11 +8,11 @@ GET_DEXES_BY_NETWORK_PATH = "networks/{}/dexes"
 GET_POOL_BY_NETWORK_AND_ADDRESS_PATH = "networks/{}/pools/{}"
 GET_TOP_POOLS_BY_NETWORK_PATH = "networks/{}/pools"
 GET_TOP_POOLS_BY_NETWORK_DEX_PATH = "networks/{}/dexes/{}/pools"
-GET_LATEST_POOLS_BY_NETWORK_PATH = "networks/{}/new_pools"
-GET_LATEST_POOLS_ALL_NETWORKS_PATH = "networks/new_pools"
+GET_NEW_POOLS_BY_NETWORK_PATH = "networks/{}/new_pools"
+GET_NEW_POOLS_ALL_NETWORKS_PATH = "networks/new_pools"
 
 # Tokens
-GET_TOP_POOLS_FOR_TOKEN_PATH = "networks/{}/tokens/{}/pools"
+GET_TOP_POOLS_BY_NETWORK_TOKEN_PATH = "networks/{}/tokens/{}/pools"
 GET_SPECIFIC_TOKEN_ON_NETWORK_PATH = "networks/{}/tokens/{}"
 
 # OHLCV
